601e6b89c4f6527780ffe838fff2b309e793758d
HyunWoo Lee <l2hyunwoo@gmail.com>
2023-11-09 11:57:01 +0900

    Change AbstractBuffer to Kotlin

--- MPChartLib/src/main/java/com/github/mikephil/charting/buffer/AbstractBuffer.kt
+++ MPChartLib/src/main/java/com/github/mikephil/charting/buffer/AbstractBuffer.kt
@@ -1,91 +1,79 @@
-
-package com.github.mikephil.charting.buffer;
-
-import java.util.List;
+package com.github.mikephil.charting.buffer
 
 /**
  * Buffer class to boost performance while drawing. Concept: Replace instead of
  * recreate.
- * 
+ *
  * @author Philipp Jahoda
  * @param <T> The data the buffer accepts to be fed with.
  */
-public abstract class AbstractBuffer<T> {
-
+abstract class AbstractBuffer<T>(size: Int) {
     /** index in the buffer */
-    protected int index = 0;
+    protected var index: Int = 0
 
     /** float-buffer that holds the data points to draw, order: x,y,x,y,... */
-    public final float[] buffer;
+    val buffer: MutableList<Float> = MutableList(size) { 0f }
 
     /** animation phase x-axis */
-    protected float phaseX = 1f;
+    protected var phaseX = 1f
 
     /** animation phase y-axis */
-    protected float phaseY = 1f;
+    protected var phaseY = 1f
 
     /** indicates from which x-index the visible data begins */
-    protected int mFrom = 0;
+    protected var mFrom = 0
 
     /** indicates to which x-index the visible data ranges */
-    protected int mTo = 0;
+    protected var mTo = 0
 
-    /**
-     * Initialization with buffer-size.
-     * 
-     * @param size
-     */
-    public AbstractBuffer(int size) {
-        index = 0;
-        buffer = new float[size];
+    /** limits the drawing on the x-axis */
+    fun limitFrom(from: Int) {
+        if (from < 0) {
+            mFrom = 0
+            return
+        }
+        mFrom = from
     }
 
     /** limits the drawing on the x-axis */
-    public void limitFrom(int from) {
-        if (from < 0)
-            from = 0;
-        mFrom = from;
-    }
-
-    /** limits the drawing on the x-axis */
-    public void limitTo(int to) {
-        if (to < 0)
-            to = 0;
-        mTo = to;
+    fun limitTo(to: Int) {
+        if (to < 0) {
+            mTo = 0
+            return
+        }
+        mTo = to
     }
 
     /**
      * Resets the buffer index to 0 and makes the buffer reusable.
      */
-    public void reset() {
-        index = 0;
+    fun reset() {
+        index = 0
     }
 
     /**
      * Returns the size (length) of the buffer array.
-     * 
+     *
      * @return
      */
-    public int size() {
-        return buffer.length;
-    }
+    fun size() = buffer.size
 
     /**
      * Set the phases used for animations.
-     * 
+     *
      * @param phaseX
      * @param phaseY
      */
-    public void setPhases(float phaseX, float phaseY) {
-        this.phaseX = phaseX;
-        this.phaseY = phaseY;
+    fun setPhases(phaseX: Float, phaseY: Float) {
+        this.phaseX = phaseX
+        this.phaseY = phaseY
     }
 
     /**
      * Builds up the buffer with the provided data and resets the buffer-index
      * after feed-completion. This needs to run FAST.
-     * 
+     *
      * @param data
      */
-    public abstract void feed(T data);
+    abstract fun feed(data: T)
 }
